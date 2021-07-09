c59b25e3da7365aafd7bab58d203d7860bbeddb1
taylor-wang <taylor.fei@qq.com>
2021-07-09 11:27:13 +0800

    Add files via upload

---
+++ GtinGs1Parser.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Material
+{
+    /// <summary>
+    /// GS1规范的物品号码GSTN解析
+    /// </summary>
+    public class GtinGs1Parser
+    {
+        public const int GS1_LENGTH_14 = 14;
+        public const int EAN13_LENGTH = 13;
+
+        protected string mCode;
+        protected string mGTIN = null;
+        protected bool mIsValid = false;
+
+        /// <summary>
+        /// 是否有效
+        /// </summary>
+        public bool IsValid
+        {
+            get {
+                return mIsValid;
+            }
+        }
+
+        /// <summary>
+        /// 14位GTIN
+        /// </summary>
+        public string GTIN {
+            get
+            {
+                return mGTIN;
+            }
+        }
+
+        public GtinGs1Parser(string code)
+        {
+            this.mCode = code;
+            mIsValid = Verify();
+        }
+
+        /// <summary>
+        /// 验证校验码
+        /// </summary>
+        /// <returns></returns>
+        protected bool Verify()
+        {
+            if (string.IsNullOrEmpty(mCode))
+            {
+                return false;
+            }
+
+            //只允许长度为13或14
+            if (mCode.Length < EAN13_LENGTH)
+            {
+                return false;
+            }
+            if (mCode.Length > GS1_LENGTH_14)
+            {
+                return false;
+            }
+
+            //转成14位
+            mGTIN = mCode;
+            if (mGTIN.Length < GS1_LENGTH_14)
+            {
+                mGTIN = mGTIN.PadLeft(GS1_LENGTH_14, '0');
+            }
+
+            int checkValue = ComputeCheckCode(mGTIN);
+            int checkCode = mGTIN[GS1_LENGTH_14 - 1] - '0';
+            if (checkValue == checkCode)
+            {
+                return true;
+            }
+
+            mGTIN = null;
+            return false;
+        }
+
+        /// <summary>
+        /// 计算校验码
+        /// </summary>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public int ComputeCheckCode(string code, int length = 13)
+        {
+            int[] addNumbers = new int[length];
+            //是否偶数位
+            bool isEven = (length % 2) > 0;
+            for (int i = 0; i < length; i++)
+            {
+                addNumbers[i] = code[i] - '0';
+                if (isEven)
+                {
+                    addNumbers[i] *= 3;
+                }
+
+                isEven = !isEven;
+            }
+
+            int sum = addNumbers.Sum();
+            int check = (10 - (sum % 10)) % 10;
+            return check;
+        }
+
+        /// <summary>
+        /// 取得不带校验码的12位编码
+        /// </summary>
+        /// <returns></returns>
+        public string GetNoCheck12Code()
+        {
+            return string.IsNullOrEmpty(mGTIN) ? null : mGTIN.Substring(1, 12);
+        }
+
+        /// <summary>
+        /// 取得13位GTIN编码
+        /// </summary>
+        /// <returns></returns>
+        public string GetEAN13()
+        {
+            string gtin = GetNoCheck12Code();
+            if (string.IsNullOrEmpty(gtin))
+            {
+                return null;
+            }
+
+            int check = ComputeCheckCode(gtin, 12);
+
+            return string.Format("{0}{1}", gtin, check);
+        }
+    }
+}
---
+++ UdiParser.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Material
+{
+    /// <summary>
+    /// Unique Device Identification
+    /// 基于GS1标准的UDI编制结构解析器
+    /// </summary>
+    public class UdiParser
+    {
+        /// <summary>
+        /// 应用标识符：全球贸易项目代码，GTIN (Global Trade Item Number)
+        /// </summary>
+        public const string AI_GTIN = "01";
+
+        /// <summary>
+        /// 应用标识符：物流单元内贸易项目代码
+        /// </summary>
+        public const string AI_UNIT_TIN = "02";
+
+        /// <summary>
+        /// 应用标识符：生产批号(不超过20位的字符数字)
+        /// </summary>
+        public const string AI_PRODUCT_BATCH = "10";
+
+        /// <summary>
+        /// 应用标识符：生产日期（6位数字，YYMMDD）
+        /// </summary>
+        public const string AI_PRODUCE_DATE = "11";
+
+        /// <summary>
+        /// 应用标识符：包装日期（6位数字，YYMMDD）
+        /// </summary>
+        public const string AI_PACK_DATE = "13";
+
+        /// <summary>
+        /// 应用标识符：保质期（6位数字，YYMMDD）
+        /// </summary>
+        public const string AI_QUALITY_DATE = "15";
+
+        /// <summary>
+        /// 应用标识符：失效日期（6位数字，YYMMDD）
+        /// </summary>
+        public const string AI_EXPIRE_DATE = "17";
+
+        /// <summary>
+        /// 应用标识符：序列号
+        /// </summary>
+        public const string AI_SN = "21";
+
+        /// <summary>
+        /// 应用标识符：变量贸易项目中的项目数量
+        /// </summary>
+        public const string AI_VAR_ITEM_NUM = "30";
+
+        /// <summary>
+        /// 应用标识符：物流单元内项目数量
+        /// </summary>
+        public const string AI_UNIT_ITEM_NUM = "37";
+
+        protected string mCode = null;
+        protected string mGTIN = null;
+        protected bool mIsValid = false;
+
+        /// <summary>
+        /// 应用标识列表
+        /// </summary>
+        protected Dictionary<string, string> mApplications = new Dictionary<string, string>();
+
+        /// <summary>
+        /// 是否有效
+        /// </summary>
+        public bool IsValid
+        {
+            get
+            {
+                return mIsValid;
+            }
+        }
+
+        /// <summary>
+        /// 14位GTIN
+        /// </summary>
+        public string GTIN
+        {
+            get
+            {
+                return mGTIN;
+            }
+        }
+
+        /// <summary>
+        /// 原始编码
+        /// </summary>
+        public string Code
+        {
+            get
+            {
+                return mCode;
+            }
+        }
+
+        /// <summary>
+        /// 取得应用标识
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public string this[string key]
+        {
+            get
+            {
+                if (mApplications.ContainsKey(key))
+                {
+                    return mApplications[key];
+                }
+                return null;
+            }
+        }
+
+        public UdiParser(string code)
+        {
+            mCode = code;
+            mIsValid = Parse();
+        }
+
+        /// <summary>
+        /// 对编码进行解析
+        /// </summary>
+        /// <returns></returns>
+        protected bool Parse()
+        {
+            if (string.IsNullOrEmpty(mCode))
+            {
+                return false;
+            }
+
+            //把中文括号替换为英文
+            string code = mCode.Replace('（', '(').Replace('）', ')');
+
+            code = code.Trim().Trim('(');
+            string[] parts = code.Split('(');
+            if (parts.Length < 1)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < parts.Length; i++)
+            {
+                if (string.IsNullOrEmpty(parts[i]))
+                {
+                    continue;
+                }
+                string[] pair = parts[i].Split(')');
+                if (pair.Length < 1 || pair.Length > 2)
+                {
+                    continue;
+                }
+
+                if (((pair.Length == 1) && (i == 0)) || ((pair.Length == 2) && AI_GTIN.Equals(pair[0])))
+                {
+                    //解析GTIN
+                    GtinGs1Parser gtin = new GtinGs1Parser(pair[pair.Length - 1]);
+                    if (gtin.IsValid)
+                    {
+                        mGTIN = gtin.GTIN;
+                        mApplications[AI_GTIN] = mGTIN;
+                    }
+                }
+                else if(pair.Length == 2)
+                {
+                    //解析应用
+                    if (!(string.IsNullOrEmpty(pair[0]) || string.IsNullOrEmpty(pair[1])))
+                    {
+                        mApplications[pair[0]] = pair[1];
+                    }
+                }
+            }
+
+            if (string.IsNullOrEmpty(mGTIN))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// 将6位数字的应用标识日期解析为时间类型
+        /// </summary>
+        /// <param name="dateNumber"></param>
+        /// <returns></returns>
+        public DateTime? GetDateBy6NumberInAI(string dateNumber)
+        {
+            if (string.IsNullOrEmpty(dateNumber) || (dateNumber.Length != 6))
+            {
+                return null;
+            }
+            Regex reg = new Regex(@"^\d{6}$");
+            if (!reg.IsMatch(dateNumber))
+            {
+                return null;
+            }
+
+            int year = Convert.ToInt32(dateNumber.Substring(0, 2));
+            int month = Convert.ToInt32(dateNumber.Substring(2, 2));
+            int day = Convert.ToInt32(dateNumber.Substring(4, 2));
+            if ((month < 1) || (month > 12) || (day < 1) || (day > 31))
+            {
+                return null;
+            }
+
+            return new DateTime(2000 + year, month, day);
+        }
+    }
+}
